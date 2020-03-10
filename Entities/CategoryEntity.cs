4533fb5bd648449f1a6210b70ac17de9270044b0
giovannihernandezm26@gmail.com <giovannihernandezm26@gmail.com>
2020-03-10 17:27:19 -0600

    class Sixteen

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities
+{
+    public class CategoryEntity
+    {
+        [Key]
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+       
+
+
+    }
+}
