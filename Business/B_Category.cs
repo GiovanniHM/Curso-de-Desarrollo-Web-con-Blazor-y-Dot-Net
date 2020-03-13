2e32785bd32f9e74801fdfb2889b2b2a6bd21d40
giovannihernandezm26@gmail.com <giovannihernandezm26@gmail.com>
2020-03-13 17:41:39 -0600

    Class twenty three

---
+++ Business/B_Category.cs
@@ -0,0 +1,50 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess;
+using System.Linq;
+
+namespace Business
+{
+    public class B_Category
+    {
+        //Se referencia la clase con la cual llenaremos la lista "CategoryEntity"
+        //"CategoryList" solo es un nombre caracteristico (atra vez de ese metodo llegara el 
+        //listado de la categoría)
+        public List<CategoryEntity> CategoryList()
+        {
+            //Llamado interno con un unicio y un final (abre y cierra conexión {})
+
+            using (var db = new InventaryContext())
+            {
+                //Retorna una lista de categories 
+                //Mediante una sentencia Linq
+                return db.Categories.ToList();
+            }
+        }
+
+        //Pasamos un obj como prarametro para cargar los datos (CategoryEntity objCategory)
+        public void CreateCategory(CategoryEntity objCategory)
+        {
+            using (var db = new InventaryContext())
+            {
+                //nos conectamos a la bd de acuerdo al contexto que se creo
+                //agregamos el objCategory que pasamos al metodo
+                db.Categories.Add(objCategory);
+                //Guardamos los cambios que pasamos 
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(CategoryEntity objCategory)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Categories.Update(objCategory);
+                db.SaveChanges();
+            }
+
+        }
+    }
+}
